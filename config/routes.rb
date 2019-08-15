815400477ac9701a85d5304cf1e70703875841e2
kimanhbd-1794 <dang.kim.anhb@sun-asterisk.com>
2019-08-15 20:38:54 +0700

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  get '/signup', to: 'users#new'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources:users
+  post '/signup',  to: 'users#create'
 end
